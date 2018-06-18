0c243caa84109fd55b5606ffe24e4b4f796c422a
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2018-06-18 16:19:44 +0900

    セッションを発行する

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 const httpProxy = require('http-proxy');
 const http = require('http');
-
+const cookie = require('cookie');
 const server = http.createServer();
 const proxy = httpProxy.createServer();
 const DB = require('./module/DB');
 let db = new DB;
 
 server.on('request',(req, res)=>{
-
-	//if(req.headers['cookie'] != undefined){
-	//	const cookie = req.headers['cookie'];
-	//	console.log(cookie);
-	//}
-	console.log(req.getHeader);
-	//初回アクセス
-	const sesstion_id = db.newSession();
-	res.setHeader('Set-Cookie',  [`Kishimen_id=${sesstion_id}`]);
+	let c = req.headers.cookie || ""
+	let sesstion_id = cookie.parse(c).Kishimen_id || db.newSession();
+	console.log(sesstion_id);//dubug
+	res.setHeader('Set-Cookie', [`Kishimen_id=${sesstion_id}; max-age=${60 * 30}`]);
 	proxy.web(req, res, {target : {host : 'localhost',port : '8080'}});
 });
+
 server.listen(9000, ()=>{
 	console.log('server listen...');
 });
+
