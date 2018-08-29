f9da9e11ac5c878dd0ce0b842af3e25d82779d96
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2018-08-29 16:47:13 +0900

    use log class

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const server = http.createServer();
 const proxy = httpProxy.createServer();
 const DB = require('module/DB');
 let db = new DB;
+const Logger = require('module/Log');
+let logger = new Logger('log/Kisimen/log.log', 'debug').logger;
 
 server.on('request',(req, res)=>{
 	let c = req.headers.cookie || ""
 	let sesstion_id = cookie.parse(c).Kishimen_id || db.newSession();
-	console.log(sesstion_id);//dubug
+	logger.info('session start : ' + sesstion_id);
 	res.setHeader('Set-Cookie', [`Kishimen_id=${sesstion_id}; max-age=${60 * 30}`]);
 	proxy.web(req, res, {target : {host : 'localhost',port : '8080'}});
 });
 
 server.listen(9000, ()=>{
-	console.log('server listen...');
+	logger.info('server listen : http://localhost:9000');
 });
 
