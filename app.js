2672e56becfabfbe846fe7a794a80900ac72f3a1
Hayato Doi <b1517914@planet.kanazawa-it.ac.jp>
2018-08-29 18:14:38 +0900

    logger -> sysLogger & accessLogger

--- app.js
+++ app.js
@@ -6,20 +6,21 @@ const proxy = httpProxy.createServer();
 const Session = require('module/Session');
 let session = new Session;
 const Logger = require('module/Log');
-let logger = new Logger('log/Kisimen/log.log', 'debug').logger;
+let sysLogger = new Logger('log/Kisimen/log.log', 'debug').logger;
+let accessLogger = new Logger('log/web/access.log', 'debug').logger;
 
 server.on('request',(req, res)=>{
 	let c = req.headers.cookie || ""
 	let sesstion_id = cookie.parse(c).Kishimen_id
 	if(sesstion_id == undefined){
 		sesstion_id = session.new();
-		logger.info('session start : ' + sesstion_id);
+		sysLogger.info('session start : ' + sesstion_id);
 	}
 	res.setHeader('Set-Cookie', [`Kishimen_id=${sesstion_id}; max-age=${60 * 30}`]);
 	proxy.web(req, res, {target : {host : 'localhost',port : '8080'}});
 });
 
 server.listen(9000, ()=>{
-	logger.info('server listen : http://localhost:9000');
+	sysLogger.info('server listen : http://localhost:9000');
 });
 
